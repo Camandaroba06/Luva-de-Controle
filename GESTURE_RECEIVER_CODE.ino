70f7c9194bd7461a7a11bd1fe4f00dda67d8ae49
Cristiano Gregory Monfrim Camandaroba <88840308+Camandaroba06@users.noreply.github.com>
2023-01-27 14:49:11 -0300

    FINALIZADO - FALTAM AJUSTES DE CURVA

--- GESTURE_RECEIVER_CODE.ino
+++ GESTURE_RECEIVER_CODE.ino
@@ -1,93 +1,98 @@
-//Arduino Gesture Control Robot
-//Reciver Circuit 
-//Created BY DIY Builder
-//You need to install the required libraries before uploading the code.
-//To install the libraries first download the library from here https://github.com/nRF24/RF24 then go to sketch > include Library > ADD .ZIP File > Select the downloaded
-// libary's Zip file and you're done now. You can upload the the sketch now.
-// For more details you can contact me here www.instagram.com/diy.builder
-
-
-#include<SPI.h>
-#include<nRF24L01.h>
-#include<RF24.h>
-
-int ENA = 3;
-int ENB = 9;
-int MotorA1 = 4;
-int MotorA2 = 5;
-int MotorB1 = 6;
-int MotorB2 = 7;
-
-RF24 radio(8, 10);
-
-const byte address[6] = "00001";
-
-struct data {
-  int xAxis;
-  int yAxis;
-
+#include <SPI.h>  
+#include "RF24.h" 
+#include <Wire.h>
+#define motor1_in1 3
+#define motor1_in2 5
+#define motor2_in1 6
+#define motor2_in2 9
+RF24 myRadio (7, 8); 
+struct package
+{
+  float xAxis = 0;
+  float yAxis = 0;
+  float zAxis = 0;
 };
-data receive_data;
+
+byte addresses[][6] = {"0"}; 
 
 
 
-void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
-radio.begin();
-radio.openReadingPipe(0,address);
-radio.setPALevel(RF24_PA_MIN);
-radio.setDataRate(RF24_250KBPS);
-radio.startListening();
-pinMode(ENA, OUTPUT);
-pinMode(ENB, OUTPUT);
-pinMode(MotorA1, OUTPUT);
-pinMode(MotorA2, OUTPUT);
-pinMode(MotorB1, OUTPUT);
-pinMode(MotorB2, OUTPUT);
+typedef struct package Package;
+Package data;
+
+void setup() 
+{
+  pinMode(motor1_in1, OUTPUT);
+  pinMode(motor1_in2, OUTPUT);
+  pinMode(motor2_in1, OUTPUT);
+  pinMode(motor2_in2, OUTPUT);
+  Serial.begin(115200);
+  delay(1000);
+
+  myRadio.begin(); 
+  myRadio.setChannel(115); 
+  myRadio.setPALevel(RF24_PA_MAX);
+  myRadio.setDataRate( RF24_250KBPS ) ; 
+  myRadio.openReadingPipe(1, addresses[0]);
+  myRadio.startListening();
 }
 
 
-void loop() {
-  // put your main code here, to run repeatedly
- while(radio.available()) {
-    radio.read(&receive_data, sizeof(data));
- if(receive_data.yAxis > 400) {
-  digitalWrite(MotorA1, LOW);
-  digitalWrite(MotorA2, HIGH);
-  digitalWrite(MotorB1, HIGH);
-  digitalWrite(MotorB2, LOW);
-  analogWrite(ENA, 150);
-  analogWrite(ENB, 150);
-  
-}else if(receive_data.yAxis < 320) {
-  digitalWrite(MotorA1, HIGH);
-  digitalWrite(MotorA2, LOW);
-  digitalWrite(MotorB1, LOW);
-  digitalWrite(MotorB2, HIGH);
-  analogWrite(ENA, 150);
-  analogWrite(ENB, 150);
-} else if(receive_data.xAxis < 320){
-  digitalWrite(MotorA1, HIGH);
-  digitalWrite(MotorA2, LOW);
-  digitalWrite(MotorB1, HIGH);
-  digitalWrite(MotorB2, LOW);
-  analogWrite(ENA, 150);
-  analogWrite(ENB, 150);
-}else if(receive_data.xAxis > 400){
-  digitalWrite(MotorA1, LOW);
-  digitalWrite(MotorA2, HIGH);
-  digitalWrite(MotorB1, LOW);
-  digitalWrite(MotorB2, HIGH);
-  analogWrite(ENA, 150);
-  analogWrite(ENB, 150);
-}else {
-  digitalWrite(MotorA1, LOW);
-  digitalWrite(MotorA2, LOW);
-  digitalWrite(MotorB1, LOW);
-  digitalWrite(MotorB2, LOW);
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
+void loop()  
+{
+
+  if ( myRadio.available()) 
+  {
+    while (myRadio.available())
+    {
+      myRadio.read( &data, sizeof(data) );
+    }
+
+    if(data.yAxis < -6){
+      Serial.println("--------");
+      Serial.println("Frente");
+      digitalWrite(motor1_in1, HIGH);
+      digitalWrite(motor1_in2, LOW);
+
+      digitalWrite(motor2_in1, HIGH);
+      digitalWrite(motor2_in2, LOW);
+    }
+     else if(data.xAxis >= 7){
+      Serial.println("--------");
+      Serial.println("Direita");
+      digitalWrite(motor1_in1, LOW);
+      digitalWrite(motor1_in2, HIGH);
+
+      digitalWrite(motor2_in1, HIGH);
+      digitalWrite(motor2_in2, LOW);
+    }
+      else if(data.yAxis >= 7 ){
+      Serial.println("--------");
+      Serial.println("tras");
+      digitalWrite(motor1_in1, LOW);
+      digitalWrite(motor1_in2, HIGH);
+
+      digitalWrite(motor2_in1, LOW);
+      digitalWrite(motor2_in2, HIGH);
+    }
+    else if(data.xAxis < -7){
+      Serial.println("--------");
+      Serial.println("Esquerda");
+      digitalWrite(motor1_in1, HIGH);
+      digitalWrite(motor1_in2, LOW);
+
+      digitalWrite(motor2_in1, LOW);
+      digitalWrite(motor2_in2, HIGH);
+    }
+    else {
+      digitalWrite(motor1_in1, LOW);
+      digitalWrite(motor1_in2, LOW);
+
+      digitalWrite(motor2_in1, LOW);
+      digitalWrite(motor2_in2, LOW);      
+    }
   }
- }
+
+  
+
 }
