6ce916d17eb508a5c93ad418b22fcd7f77ec408e
Cristiano Gregory Monfrim Camandaroba <88840308+Camandaroba06@users.noreply.github.com>
2023-05-28 11:50:13 -0300

    Update GESTURE_RECEIVER_CODE.ino

--- GESTURE_RECEIVER_CODE.ino
+++ GESTURE_RECEIVER_CODE.ino
@@ -30,7 +30,7 @@ void setup()
   delay(1000);
 
   myRadio.begin(); 
-  myRadio.setChannel(115); 
+  myRadio.setChannel(81); 
   myRadio.setPALevel(RF24_PA_MAX);
   myRadio.setDataRate( RF24_250KBPS ) ; 
   myRadio.openReadingPipe(1, addresses[0]);
